7ee385076f2b8dc57e6907b03270906d16628bf8
gbogado92 <62188600+gbogado92@users.noreply.github.com>
2021-05-11 11:32:13 -0300

    Lee el valor de la marca del automóvil.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
 // Variables
 
-const resultado = document.querySelector('#resultado');
+const marca = document.querySelector('#marca');
 const year = document.querySelector('#year');
+const minimo = document.querySelector('#minimo');
+const maximo = document.querySelector('#maximo');
+const puertas = document.querySelector('#puertas');
+const transmision = document.querySelector('#transmision');
+const color = document.querySelector('#color');
+const resultado = document.querySelector('#resultado');
 
 const max = new Date().getFullYear();
 const min = max - 10;
+
+// Generar un objeto con la busqueda
+
+const datosBusqueda = {
+    marca: '',
+    year: '',
+    minimo: '',
+    maximo: '',
+    puertas: '',
+    transmision: '',
+    color: ''
+}
 
 // Eventos
 
@@ -15,6 +33,49 @@ document.addEventListener('DOMContentLoaded', () => {
     llenarSelect();
 })
 
+// Event Listener para los select de búsqueda 
+marca.addEventListener( 'change', e => {
+    datosBusqueda.marca = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+year.addEventListener( 'change', e => {
+    datosBusqueda.year = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+minimo.addEventListener( 'change', e => {
+    datosBusqueda.minimo = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+maximo.addEventListener( 'change', e => {
+    datosBusqueda.maximo = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+puertas.addEventListener( 'change', e => {
+    datosBusqueda.puertas = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+
+transmision.addEventListener( 'change', e => {
+    datosBusqueda.transmision = e.target.value;
+
+    console.log(datosBusqueda);
+});
+
+color.addEventListener( 'change', e => {
+    datosBusqueda.color = e.target.value;
+
+    console.log(datosBusqueda);
+});
 
 
 // Funciones 
